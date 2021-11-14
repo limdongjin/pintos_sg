2bf7f9fce0fe51a11d76ad3cd0028226b347d7cf
limdongjin <geniuslim27@gmail.com>
2021-11-14 02:53:55 -0800

    [PRJ4] some code cleanup (3fail)

--- src/vm/page.c
+++ src/vm/page.c
@@ -1,5 +1,4 @@
 #include "page.h"
-//#include "frame.h"
 #include "swap.h"
 #include "threads/palloc.h"
 #include "threads/thread.h"
@@ -7,14 +6,16 @@
 #include "threads/vaddr.h"
 #include "threads/interrupt.h"
 
-struct hash page_table;
+/* Declarations of Helper Functions */
 
+struct hash page_table;
 static uint32_t calc_page_idx (const struct page_entry *page);
 static struct hash_elem *get_hash_elem (void *va, uint32_t pid);
 static bool page_less_func (const struct hash_elem *a,
                             const struct hash_elem *b, void *aux);
 static unsigned page_hash_func (const struct hash_elem *a,
                                 void *aux);
+static struct page_entry* make_page_by_(void* va, void* pa, bool writable);
 
 static bool
 page_less_func (const struct hash_elem *a,
@@ -23,12 +24,11 @@ page_less_func (const struct hash_elem *a,
            < calc_page_idx(hash_entry(b, struct page_entry, elem));
 }
 
-static unsigned
-page_hash_func (const struct hash_elem *a, void *aux) {
-    uint32_t idx = calc_page_idx(hash_entry(a,
-                                        struct page_entry,
-                                                elem));
-    return hash_bytes (&idx, sizeof idx);
+/* Definitions of Functions of vm/page.h */
+
+uint32_t
+calc_page_number (void* addr) {
+    return ((uint32_t)addr& 0xfffff000)>> 12;
 }
 
 void
@@ -38,11 +38,8 @@ init_page_table () {
     lock_init (&page_lock);
 }
 
-uint32_t calc_page_number (void* addr) {
-    return ((uint32_t)addr& 0xfffff000)>> 12;
-}
-
-struct page_entry *get_page_by_ (void *va, uint32_t pid) {
+struct
+page_entry *get_page_by_ (void *va, uint32_t pid) {
     struct page_entry* ret = NULL;
     struct hash_elem* he;
     lock_acquire (&page_lock);
@@ -55,41 +52,8 @@ struct page_entry *get_page_by_ (void *va, uint32_t pid) {
     return ret;
 }
 
-static struct hash_elem*
-get_hash_elem (void *va, uint32_t pid) {
-    struct page_entry page;
-    page.vaddr = calc_page_number(va);
-    page.pid = pid;
-    return hash_find(&page_table, &page.elem);
-}
-
-static struct page_entry* make_page_by_(void* va, void* pa, bool writable);
-static struct page_entry*
-make_page_by_(void* va, void* pa, bool writable){
-            struct page_entry* page;
-            ASSERT (va != NULL);
-            page = malloc (sizeof (struct page_entry));
-            ASSERT (page != NULL);
-    page->is_pinned = false;
-    page->writable = writable;
-    page->vaddr = calc_page_number(va);
-    page->pid = thread_tid ();
-    page->elem.list_elem.next = NULL;
-    page->elem.list_elem.prev = NULL;
-    page->t = thread_current();
-            if(pa == frame_for_swap) {
-                page->paddr = 0;
-                page->swap_idx = swap_out (pa);  // knpage는 load_segment에서 채워짐
-                page->disk = true;
-            }else{
-                page->paddr = calc_page_number(pa);
-                page->swap_idx = -1;
-                page->disk = false;
-            }
-
-            return page;
-}
-void insert_page (void *va, void *pa, bool writable) {
+void
+insert_page (void *va, void *pa, bool writable) {
     struct page_entry* page = make_page_by_(va, pa, writable);
 
     lock_acquire (&page_lock);
@@ -99,7 +63,8 @@ void insert_page (void *va, void *pa, bool writable) {
     lock_release (&page_lock);
 }
 
-bool delete_page (void *va, uint32_t pid) {
+bool
+delete_page (void *va, uint32_t pid) {
     lock_acquire (&page_lock);
     struct hash_elem *target = get_hash_elem (va, pid);
     lock_release (&page_lock);
@@ -114,7 +79,8 @@ bool delete_page (void *va, uint32_t pid) {
     return true;
 }
 
-bool delete_pages_by_ (uint32_t pid) {
+bool
+delete_pages_by_ (uint32_t pid) {
     lock_acquire (&page_lock);
     struct page_entry* page;
     struct hash_elem* he;
@@ -136,7 +102,8 @@ bool delete_pages_by_ (uint32_t pid) {
     return true;
 }
 
-bool page_evict_frame () {
+bool
+page_evict_frame () {
     struct hash_iterator it;
     struct page_entry *page = NULL;
 
@@ -172,6 +139,50 @@ page_set_swap (void *va, void *pa, uint32_t pid) {
                                             elem);
     page->swap_idx = -1;
     page->paddr = calc_page_number(pa);
+}
+
+/* Definitions of Helper Functions */
+
+static unsigned
+page_hash_func (const struct hash_elem *a, void *aux) {
+    uint32_t idx = calc_page_idx(hash_entry(a,
+                                            struct page_entry,
+                                            elem));
+    return hash_bytes (&idx, sizeof idx);
+}
+
+static struct hash_elem*
+get_hash_elem (void *va, uint32_t pid) {
+    struct page_entry page;
+    page.vaddr = calc_page_number(va);
+    page.pid = pid;
+    return hash_find(&page_table, &page.elem);
+}
+
+static struct page_entry*
+make_page_by_(void* va, void* pa, bool writable){
+    struct page_entry* page;
+    ASSERT (va != NULL);
+    page = malloc (sizeof (struct page_entry));
+    ASSERT (page != NULL);
+    page->is_pinned = false;
+    page->writable = writable;
+    page->vaddr = calc_page_number(va);
+    page->pid = thread_tid ();
+    page->elem.list_elem.next = NULL;
+    page->elem.list_elem.prev = NULL;
+    page->t = thread_current();
+    if(pa == frame_for_swap) {
+        page->paddr = 0;
+        page->swap_idx = swap_out (pa);
+        page->disk = true;
+    }else{
+        page->paddr = calc_page_number(pa);
+        page->swap_idx = -1;
+        page->disk = false;
+    }
+
+    return page;
 }
 
 static uint32_t
